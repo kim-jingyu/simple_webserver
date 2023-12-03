0edf328b1253a2e59eba3bb57b2282d4ad557e26
kim-jingyu <swc05161@gmail.com>
2023-12-03 14:01:12 +0000

    feat: 회원가입 주소 등록

--- signup_func.php
+++ signup_func.php
@@ -1,33 +1,44 @@
 <?php
 	require 'db_info.php';
 
-	$id = $_POST['UserId'];
-	$password = $_POST['Password'];
-	$username = $_POST['UserName'];
-	$userinfo = $_POST['UserInfo'];
+	$db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-	if ($id != NULL and $password != NULL and $username != NULL) {
+	$id = mysqli_real_escape_string($db_conn, $_POST['userId']);
+	$password = mysqli_real_escape_string($db_conn, $_POST['password']);
+	$encoded_pw = md5($password);
+	$username = mysqli_real_escape_string($db_conn, $_POST['userName']);
+	$userinfo = mysqli_real_escape_string($db_conn, $_POST['userInfo']);
 
-		$db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-		
-		$encoded_pw = md5($password);
 
-		$sql = "INSERT INTO member (user_id, user_pw, user_name, user_level, user_info) VALUES ('$id', '$encoded_pw', '$username', 'student', '$userinfo');";
-
-		$result = mysqli_query($db_conn, $sql);
-
-		if ($result) {
-			echo "<script>alert('SignUp Succeeded!')</script>";
-			echo "<script>window.location.href='index.php';</script>";
-		} else {
-			echo "<script>alert('This ID already exists')</script>";
-			echo "<script>window.location.href='signup.php';</script>";
-		}
-		mysqli_close($db_conn);
-	} else {
-		echo "<script>alert('Input ID and Password and Username')</script>";
-		echo "<script>window.location.href='signup.php';</script>";
+	if ($id == NULL or $password == NULL or $username == NULL) {
+		echo "<script>alert('ID, Password, Username 칸을 입력해주세요.')</script>";
+		echo "<script>window.location.href='signup.html';</script>";
 	}
+	
+	$address = trim(mysqli_real_escape_string($db_conn, $_POST['address']));
+	$encryption_key = 'secret_key';
+	$encrypted_address = openssl_encrypt($address, 'aes-256-cbc', $encryption_key, OPENSSL_ZERO_PADDING, '1234567890123456');
+
+	$select_sql = "select * from member where user_id = '$id'";
+	$select_result = mysqli_num_rows(mysqli_query($db_conn, $select_sql));
+	if ($select_result > 0) {
+		echo "<script>alert('ID가 중복됩니다!')</script>";
+		echo "<script>window.location.href='signup.html';</script>";
+		exit();
+	}
+
+	$insert_sql = "INSERT INTO member (user_id, user_pw, user_name, user_level, user_info, user_address) VALUES ('$id', '$encoded_pw', '$username', 'student', '$userinfo', '$encrypted_address');";
+
+	$insert_result = mysqli_query($db_conn, $insert_sql);
+
+	if ($insert_result) {
+		echo "<script>alert('SignUp Succeeded!')</script>";
+		echo "<script>window.location.href='index.php';</script>";
+	} else {
+		echo "<script>alert('SignUp Fail!')</script>";
+		echo "<script>window.location.href='signup.html';</script>";
+	}
+	mysqli_close($db_conn);
 
 	exit();
 ?>
