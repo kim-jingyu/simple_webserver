1b1649c157c8a02c8dbca179e6e2e0eed9f4fefa
kim-jingyu <swc05161@gmail.com>
2023-12-13 12:01:14 +0000

    fix: 마이페이지 - 아이디, 비밀번호 보안 설정 추가 및 비밀번호 검증 로직 추가중

--- mypage/change_id.php
+++ mypage/change_id.php
@@ -14,13 +14,21 @@
         die('데이터베이스 오류발생!'.mysqli_connect_error());
     }
 
-    $old_id = $_POST['OldId'];
-    $new_id = $_POST['NewId'];
+    $old_id = $conn -> real_escape_string(filter_var(strip_tags($_POST['OldId']), FILTER_SANITIZE_SPECIAL_CHARS));
 
-    $update_sql = "update member set user_id = '$new_id' where user_id = '$old_id'";
-    $result = $conn -> query($update_sql);
+    if ($old_id != $_SESSION['loginId']) {
+        echo "<script>alert('ID 수정에 실패했습니다!');</script>";
+        echo "<script>location.replace('/mypage/mypage.php');</script>";
+    }
 
-    if ($result) {
+    $new_id = $conn -> real_escape_string(filter_var(strip_tags($_POST['NewId']), FILTER_SANITIZE_SPECIAL_CHARS));
+
+    $member_update_sql = "update member set user_id = '$new_id' where user_id = '$old_id'";
+    $board_update_sql = "update board set user_id = '$new_id' where user_id = '$old_id'";
+    $member_result = $conn -> query($member_update_sql);
+    $board_result = $conn -> query($board_update_sql);
+
+    if ($member_result && $board_result) {
         session_regenerate_id();    // ID 자동 갱신
         $_SESSION['loginId'] = $new_id;
         echo "<script>alert('ID가 수정되었습니다!');</script>";
--- mypage/change_pw.php
+++ mypage/change_pw.php
@@ -7,13 +7,29 @@
         die('데이터베이스 오류발생!'.mysqli_connect_error());
     }
 
-    $old_pw = md5($_POST['OldPw']);
-    $new_pw = md5($_POST['NewPw']);
+    $old_pw = $conn -> real_escape_string(filter_var(strip_tags(md5($_POST['OldPw'])), FILTER_SANITIZE_SPECIAL_CHARS));
+
+    $login_id = $_SESSION['loginId'];
+    $select_pw_sql = "select pw from member where user_id = '$loginId'";
+    $select_pw_result = $conn -> query($select_pw_sql);
+
+    if (!mysqli_num_rows($select_pw_result)) {
+        echo "<script>alert('계정 정보가 없습니다!');</script>";
+        echo "<script>location.replace('/mypage/mypage.php');</script>";
+    }
+
+    $row = $select_pw_result -> fetch_assoc();
+    if ($old_pw != $row['user_pw']) {
+        echo "<script>alert('비밀번호가 일치하지 않습니다!');</script>";
+        echo "<script>location.replace('/mypage/mypage.php');</script>";
+    }
+
+    $new_pw = $conn -> real_escape_string(filter_var(strip_tags(md5($_POST['NewPw'])), FILTER_SANITIZE_SPECIAL_CHARS));
 
     $update_sql = "update member set user_pw = '$new_pw' where user_pw = '$old_pw'";
-    $result = $conn -> query($update_sql);
+    $update_result = $conn -> query($update_sql);
 
-    if ($result) {
+    if ($update_result) {
         echo "<script>alert('PW가 수정되었습니다!');</script>";
     } else {
         echo "<script>alert('PW 수정에 실패했습니다!');</script>";
