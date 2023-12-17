5aa96f33718e658873bf67773e6e3e7124854b23
kim-jingyu <swc05161@gmail.com>
2023-12-17 11:02:08 +0000

    chore: style.css 적용 중

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Home</title>
 </head>
 
@@ -19,16 +17,18 @@
         require_once $_SERVER['DOCUMENT_ROOT'].'/jwt/jwt.php';
         $id = getToken($_COOKIE['JWT'])['user'];
     ?>
-    <h2><?php echo "어서오세요. $id"; ?>님!</h2>
-    <button type="button" class="btn btn-secondary" onclick="location.href='/logout/logout.php'">
-        로그아웃
-    </button>
-    <form action="/board/board.php">
-        <p><input type="submit" value="메뉴"></p>
-    </form>
-    <form action="/mypage/mypage.php">
-        <p><input type="submit" value="마이페이지"></p>
-    </form>
+    <div class="container">
+        <h2><?php echo "어서오세요. $id"; ?>님!</h2>
+        <button type="button" class="logout_btn" onclick="location.href='/logout/logout.php'">
+            로그아웃
+        </button>
+        <form action="/board/board.php">
+            <p><input type="submit" value="메뉴"></p>
+        </form>
+        <form action="/mypage/mypage.php">
+            <p><input type="submit" value="마이페이지"></p>
+        </form>
+    </div>
 </body>
 
 </html>
