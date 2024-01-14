9319b385193f8c8f032698df084728a91ec9eee0
kim-jingyu <swc05161@gmail.com>
2024-01-14 14:18:52 +0000

    delete: DBConnect

--- application/repository/DBConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require $_SERVER['DOCUMENT_ROOT'].'/application/config/db/db_info.php';
-
-    $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
-
-    if (mysqli_connect_errno()) {
-        die('데이터베이스 오류 발생'.mysqli_connect_error());
-    }
-?>
