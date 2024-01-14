702aa9fa744f3469b6d5fba9212f82ab356af4bf
kim-jingyu <swc05161@gmail.com>
2024-01-14 16:50:55 +0000

    fix: 로그아웃 기능, 회원가입 페이지

--- application/service/logout/LogoutService.php
+++ application/service/logout/LogoutService.php
@@ -1,4 +1,4 @@
 <?php
     setcookie('JWT', NULL, time()-30*60, "/");
-    echo "<script>location.replace('/login/login.html');</script>";
+    echo "<script>location.replace('/application/view/login/login.html');</script>";
 ?>
