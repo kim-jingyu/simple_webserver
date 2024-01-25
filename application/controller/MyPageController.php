6437287ef18c02bcf44d20900ae07d06e4c629db
kim-jingyu <swc05161@gmail.com>
2024-01-25 17:58:28 +0000

    fix: mypage - change id, change pw

--- application/controller/MyPageController.php
+++ application/controller/MyPageController.php
@@ -1,33 +1,32 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/application/config/jwt/JwtManager.php';
-    require_once $_SERVER['DOCUMENT_ROOT'].'/application/service/member/MemberService.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/application/service/mypage/MypageService.php';
 
     function close($message) {
-        $mypageUrl = '/application/view/mypage/mypage.php';
-        echo "<script>alert('{$message}')</script>";
-        echo "<script>location.replace('{$mypageUrl}');</script>";
+        echo "<script>alert('$message')</script>";
+        echo "<script>location.replace('/application/view/mypage/mypage.php');</script>";
         exit();
     }
 
-    $memberService = new MemberService();
+    $myPageService = new MypageService();
 
     if ($_REQUEST['changeId']) {
         $newId = filter_var(strip_tags($_POST['newId']), FILTER_SANITIZE_SPECIAL_CHARS);
         $originalId = getToken($_COOKIE['JWT'])['user'];
         if ($newId == $originalId) {
             echo "<script>alert('ID가 같습니다!');</script>";
-            echo "<script>location.replace('{$mypageUrl}');</script>";
+            echo "<script>location.replace('/application/view/mypage/mypage.php');</script>";
             exit;
         }
         
-        $message = $memberService->changeId($newId, $originalId);
+        $message = $myPageService->changeId($newId, $originalId);
         close($message);
     } else if ($_REQUEST['changePw']) {
         $originalId = getToken($_COOKIE['JWT'])['user'];
         $oldPw = filter_var(strip_tags(md5($_POST['oldPw'])), FILTER_SANITIZE_SPECIAL_CHARS);
         $newPw = filter_var(strip_tags(md5($_POST['newPw'])), FILTER_SANITIZE_SPECIAL_CHARS);
 
-        $message = $memberService->changePw($originalId, $oldPw, $newPw);
+        $message = $myPageService->changePw($originalId, $oldPw, $newPw);
         close($message);
     }
 ?>
--- application/service/mypage/MypageService.php
+++ application/service/mypage/MypageService.php
@@ -1,35 +1,39 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/application/repository/member/MemberRepository.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/application/repository/board/BoardRepository.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/application/config/jwt/JwtManager.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/application/config/jwt/JwtManager.php';
     
     class MypageService {
-        private $memberRepository;
-        private $boardRepository;
-
         public function __construct() {
-            $this->memberRepository = new MemberRepository();
-            $this->boardRepository = new BoardRepository();
         }
 
         public function changeId($newId, $originalId) {
+            $memberRepository = new MemberRepository();
+            $boardRepository = new BoardRepository();
+
             try {
                 $memberRepository->updateId($newId, $originalId);
                 $boardRepository->updateId($newId, $originalId);
                 
-                $member = $memberRepository->findById($newId);
-                $jwt = createToken($member['user_id'], $member['user_name']);
+                $result = $memberRepository->findById($newId);
+                $row = $result->fetch_assoc();
+                $userId = $row['user_id'];
+                $userName = $row['user_name'];
+                $jwt = createToken($userId, $userName);
                 setcookie('JWT', $jwt, time() + 30 * 60, "/");
                 return "ID가 수정되었습니다!";
             } catch (Exception $e) {
-                echo $e->getMessage();
                 return "ID 수정에 실패했습니다!";
             }
         }
     
         public function changePw($originalId, $oldPw, $newPw) {
-            $originalPw = $memberRepository->findById($originalId)['user_pw'];
-            if ($oldPw != $originalPw) {
+            $memberRepository = new MemberRepository();
+
+            $result = $memberRepository->findById($originalId);
+            $row = $result->fetch_assoc();
+            $originalPw = $row['user_pw'];
+            if (password_verify($oldPw, $originalPw)) {
                 return "비밀번호가 일치하지 않습니다!";
             }
     
@@ -37,7 +41,6 @@
                 $memberRepository->updatePw($newPw, $oldPw);
                 return "PW가 수정되었습니다!";
             } catch (Exception $e) {
-                echo $e->getMessage();
                 return "PW 수정에 실패했습니다!";
             }
         }
