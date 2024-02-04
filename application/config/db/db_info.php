650a9d6762a761c498ffa797cfc5a70ca409ded7
kim-jingyu <swc05161@gmail.com>
2024-02-04 19:38:51 +0000

    fix: db_info

--- application/config/db/db_info.php
+++ application/config/db/db_info.php
@@ -4,14 +4,14 @@
         }
 
         public static function getInfo() {
-            // $db_host = getenv("DB_HOST");
-            // $db_username = getenv("DB_USERNAME");
-            // $db_password = getenv("DB_PASSWORD");
-            // $db_name = getenv("DB_NAME");
-            $db_host = "localhost";
-            $db_username = "jingyu";
-            $db_password = "5478";
-            $db_name = "simpledb";
+            $db_host = getenv("DB_HOST");
+            $db_username = getenv("DB_USERNAME");
+            $db_password = getenv("DB_PASSWORD");
+            $db_name = getenv("DB_NAME");
+            // $db_host = "localhost";
+            // $db_username = "jingyu";
+            // $db_password = "5478";
+            // $db_name = "simpledb";
 
             $info = array($db_host, $db_username, $db_password, $db_name);
             return $info;
