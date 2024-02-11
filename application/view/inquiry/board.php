c70e90f1a2bcafcea41fc602085ec6beba4c0eb1
kim-jingyu <swc05161@gmail.com>
2024-02-11 10:38:38 +0000

    fix: inquiry 디자인 수정

--- application/view/inquiry/board.php
+++ application/view/inquiry/board.php
@@ -21,77 +21,79 @@
 </head>
 <body>
     <div class="container">
-        <h1>문의게시판</h1>
+        <h1 class="board-title">문의게시판</h1>
         <div class="board">
-            <form class="funcs" action="" method="get">
-                <div>
-                    <select name="sort" onchange="this.form.submit()">
-                        <option value=""  <?php if(isset($sort)) {echo "disabled"; } ?>>정렬 옵션</option>
-                        <option value="author" <?php if($sort == 'author') {echo "selected"; } ?>>작성자순</option>
-                        <option value="date" <?php if($sort == 'date') {echo "selected"; } ?>>날짜순</option>
-                    </select>
-                </div>
-                <div class="search">
-                    <input class="search_text" type="text" name="search" value="<?php echo $searchWord; ?>" placeholder="검색">
-                    <input class="btn" type="submit" value="검색">
-                </div>
-                <div class="date">
-                    <label for="dateValue">날짜:</lable>
-                    <input type="date" id="dateValue" name="dateValue" value="<?php echo $dateValue; ?>">    
-                </div>
-            </form>
-        </div>
-        <table>
-            <tr>
-                <th></th>
-                <th>제목</th>
-                <th>작성자</th>
-            </tr>
-            <?php
-                if (mysqli_num_rows($result)) {
-                    while ($row = mysqli_fetch_array($result)) {
-                        echo '<tr>';
-                        echo '<td>'.$row['id'].'</td>';
-                        echo '<td><a href="/application/view/inquiry/board_view.php?boardId='.$row['id'].'">'.$row['title'].'</a></td>';
-                        echo '<td>'.$row['writer_name'].'</td>';
-                        echo '</tr>';
-                    }
-                    echo '</table>';
-                    echo '<p class="page"> [ ';
-                    if ($blockNow > 1) {
-                        $prevBlockStart = $blockNow - 5;
-                        if ($prevBlockStart == 0) {
-                            $prevBlockStart = 1;
+            <div>
+                <form class="funcs" action="" method="get">
+                    <div>
+                        <select name="sort" onchange="this.form.submit()">
+                            <option value=""  <?php if(isset($sort)) {echo "disabled"; } ?>>정렬 옵션</option>
+                            <option value="author" <?php if($sort == 'author') {echo "selected"; } ?>>작성자순</option>
+                            <option value="date" <?php if($sort == 'date') {echo "selected"; } ?>>날짜순</option>
+                        </select>
+                    </div>
+                    <div class="search">
+                        <input class="search_text" type="text" name="search" value="<?php echo $searchWord; ?>" placeholder="검색">
+                        <input class="btn" type="submit" value="검색">
+                    </div>
+                    <div class="date-box">
+                        <label for="dateValue">날짜:</lable>
+                        <input type="date" class="date" id="dateValue" name="dateValue" value="<?php echo $dateValue; ?>">    
+                    </div>
+                </form>
+            </div>
+            <table>
+                <tr>
+                    <th></th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                </tr>
+                <?php
+                    if (mysqli_num_rows($result)) {
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo '<tr>';
+                            echo '<td>'.$row['id'].'</td>';
+                            echo '<td><a class="link" href="/application/view/inquiry/board_view.php?boardId='.$row['id'].'">'.$row['title'].'</a></td>';
+                            echo '<td>'.$row['writer_name'].'</td>';
+                            echo '</tr>';
                         }
-                        echo '<a href="?page='.$prevBlockStart.'&search='.$searchWord.'&dateValue'.$dateValue.'">이전 페이지</a>"';
-                    }
-
-                    for ($pageNum = $blockNow + 1; $pageNum <= min($blockNow + 5, $totalPages); $pageNum++) {
-                        if ($pageNum == $pageNow) {
-                            echo $pageNum;
-                        } else {
-                            echo '<a href="?page='.$pageNum.'&search='.$searchWord.'&dateValue='.$dateValue.'">'.$pageNum.'</a>';
+                        echo '</table>';
+                        echo '<p class="page"> [ ';
+                        if ($blockNow > 1) {
+                            $prevBlockStart = $blockNow - 5;
+                            if ($prevBlockStart == 0) {
+                                $prevBlockStart = 1;
+                            }
+                            echo '<a class="link" href="?page='.$prevBlockStart.'&search='.$searchWord.'&dateValue'.$dateValue.'">이전 페이지</a>"';
                         }
-                        echo ' ';
-                    }
 
-                    if ($blockNow + 5 < $totalPages) {
-                        $nextBlockStart = $blockNow + 6;
-                        echo '<a href="?page='.$nextBlockStart.'&search='.$searchWord.'&dateValue='.$dateValue.'">다음 페이지</a>';
+                        for ($pageNum = $blockNow + 1; $pageNum <= min($blockNow + 5, $totalPages); $pageNum++) {
+                            if ($pageNum == $pageNow) {
+                                echo $pageNum;
+                            } else {
+                                echo '<a class="link" href="?page='.$pageNum.'&search='.$searchWord.'&dateValue='.$dateValue.'">'.$pageNum.'</a>';
+                            }
+                            echo ' ';
+                        }
+
+                        if ($blockNow + 5 < $totalPages) {
+                            $nextBlockStart = $blockNow + 6;
+                            echo '<a class="link" href="?page='.$nextBlockStart.'&search='.$searchWord.'&dateValue='.$dateValue.'">다음 페이지</a>';
+                        }
+                        echo ' ]</p>';
+                    } else {
+                        echo '</table>';
+                        echo "게시물이 없습니다.";
                     }
-                    echo ' ]</p>';
-                } else {
-                    echo '</table>';
-                    echo "게시물이 없습니다.";
-                }
-            ?>
-        <div class="footer">
-            <form action="board_write.php" method="post">
-                <input class="btn" type="submit" name="writeBoard" value="게시글 작성">
-            </form>
-            <form action="/application/view/login/login.html">
-                <input class="btn" type="submit" value="뒤로">
-            </form>
+                ?>
+            <div class="footer">
+                <form action="board_write.php" method="post">
+                    <input class="btn" type="submit" name="writeBoard" value="게시글 작성">
+                </form>
+                <form action="/application/view/login/login.html">
+                    <input class="btn" type="submit" value="뒤로">
+                </form>
+            </div>
         </div>
     </div>
 </body>
