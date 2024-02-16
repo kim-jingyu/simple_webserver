60b10ed24c5f814aaeed274a75d9eb3769d88c68
kim-jingyu <swc05161@gmail.com>
2024-02-16 20:19:33 +0000

    refactor: comment - pdo transaction 적용

--- application/controller/comment/CommentController.php
+++ application/controller/comment/CommentController.php
@@ -1,74 +1,102 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/application/repository/comment/CommentRepository.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/application/connection/DBConnectionUtil.php';
 
     class CommentController {
         public function __construct() {
         }
 
         public function write($commenterId, $body, $boardId) {
+            $conn = DBConnectionUtil::getConnection();
+            $commentRepository = new CommentRepository();
             try {
-                $commentRepository = new CommentRepository();
+                $conn->beginTransaction();
 
                 $dateValue = date("Y-m-d H:i:s");
-                $commentRepository->write($commenterId, $body, $dateValue, $boardId);
+                $commentRepository->write($conn, $commenterId, $body, $dateValue, $boardId);
 
+                $conn->commit();
                 echo "<script>alert('댓글 작성완료!');</script>";
             } catch (Exception $e) {
+                $conn->rollback();
                 echo "<script>alert('댓글 작성중 문제가 발생했습니다!');</script>";
             } finally {
+                if ($conn != null) {
+                    $conn = null;
+                }
                 echo "<script>location.replace('/application/view/board/board_view.php?boardId=$boardId');</script>";
             }
         }
 
         public function fix($body, $id, $boardId) {
+            $conn = DBConnectionUtil::getConnection();
+            $commentRepository = new CommentRepository();
             try {
-                $commentRepository = new CommentRepository();
-                $commenterId = $commentRepository->findCommenterIdById($id);
+                $conn->beginTransaction();
+                $commenterId = $commentRepository->findCommenterIdById($conn, $id);
 
                 $userId = getToken($_COOKIE['JWT'])['user'];
                 if ($commenterId != $userId) {
                     throw new Exception;
                 }
 
-                $commentRepository->fix($body, $id);
+                $commentRepository->fix($conn, $body, $id);
 
+                $conn->commit();
                 echo "<script>alert('댓글 수정완료!');</script>";
             } catch (Exception $e) {
+                $conn->rollback();
                 echo "<script>alert('댓글 수정중 문제가 발생했습니다!');</script>";
             } finally {
+                if ($conn != null) {
+                    $conn = null;
+                }
                 echo "<script>location.replace('/application/view/board/board_view.php?boardId=$boardId');</script>";
             }
         }
 
         public function getComments($boardId) {
+            $conn = DBConnectionUtil::getConnection();
+            $commentRepository = new CommentRepository();
             try {
-                $commentRepository = new CommentRepository();
+                $conn->beginTransaction();
 
-                $result = $commentRepository->findAllByBoard($boardId);
-                return $result;
+                $row = $commentRepository->findAllByBoard($conn, $boardId);
+                $conn->commit();
+                return $row;
             } catch (Exception $e) {
                 echo "<script>alert('댓글을 가져오는 도중에 문제가 발생했습니다!');</script>";
             } finally {
+                if ($conn != null) {
+                    $conn = null;
+                }
                 echo "<script>location.replace('/application/view/board/board_view.php?boardId=$boardId');</script>";
             }
         }
 
         public function delete($commentId, $boardId) {
+            $conn = DBConnectionUtil::getConnection();
+            $commentRepository = new CommentRepository();
             try {
-                $commentRepository = new CommentRepository();
-                $commenterId = $commentRepository->findCommenterIdById($commentId);
+                $conn->beginTransaction();
+                $commenterId = $commentRepository->findCommenterIdById($conn, $commentId);
 
                 $userId = getToken($_COOKIE['JWT'])['user'];
                 if ($commenterId != $userId) {
                     throw new Exception;
                 }
 
-                $commentRepository->delete($commentId);
+                $commentRepository->delete($conn, $commentId);
 
+                $conn->commit();
                 echo "<script>alert('댓글 삭제완료!');</script>";
             } catch (Exception $e) {
+                $conn->rollback();
                 echo "<script>alert('댓글 삭제중 문제가 발생했습니다!');</script>";
             } finally {
+                if ($conn != null) {
+                    $conn = null;
+                }
                 echo "<script>location.replace('/application/view/board/board_view.php?boardId=$boardId');</script>";
             }
         }
--- application/repository/comment/CommentRepository.php
+++ application/repository/comment/CommentRepository.php
@@ -1,166 +1,128 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'].'/application/connection/DBConnectionUtil.php';
-    
     class CommentRepository {
         public function __construct() {
         }
 
-        public function write($commenterId, $body, $dateValue, $boardId) {
-            $conn = null;
+        public function write($conn, $commenterId, $body, $dateValue, $boardId) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "INSERT INTO comment(commenter_id, body, comment_date, board_id) VALUES (?,?,?,?)";
+                $sql = "INSERT INTO comment(commenter_id, body, comment_date, board_id) VALUES (:commenterId,:body,:dateValue,:boardId)";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("sssi", $commenterId, $body, $dateValue, $boardId);
+                $stmt->bindValue(":commenterId", $commenterId);
+                $stmt->bindValue(":body", $body);
+                $stmt->bindValue(":dateValue", $dateValue);
+                $stmt->bindValue(":boardId", $boardId, PDO::PARAM_INT);
                 $stmt->execute();
-                $commentId = $conn->insert_id;
+
+                $commentId = $conn->lastInsertId();
                 return $commentId;
-            } catch (Exception $e) {
-                throw new Exception("Write At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function fix($body, $id) {
-            $conn = null;
+        public function fix($conn, $body, $id) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "UPDATE comment SET body = ? WHERE id = ?";
+                $sql = "UPDATE comment SET body = :body WHERE id = :id";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("si", $body, $id);
+                $stmt->bindValue(":body", $body);
+                $stmt->bindValue(":id", $id, PDO::PARAM_INT);
                 $stmt->execute();
-            } catch (Exception $e) {
-                throw new Exception("Fix At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function findAllByBoard($boardId) {
-            $conn = null;
+        public function findAllByBoard($conn, $boardId) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "SELECT * FROM comment WHERE board_id = ?";
+                $sql = "SELECT * FROM comment WHERE board_id = :boardId";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $boardId);
+                $stmt->bindValue(":boardId", $boardId, PDO::PARAM_INT);
                 $stmt->execute();
-                $result = $stmt->get_result();
-                return $result;
-            } catch (Exception $e) {
-                throw new Exception("findAllByBoard At Comment - DB Exception 발생!");
+                $row = $stmt->fetch();
+                return $row;
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function findCommenterIdById($id) {
-            $conn = null;
+        public function findCommenterIdById($conn, $id) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "SELECT commenter_id FROM comment WHERE id = ?";
+                $sql = "SELECT commenter_id FROM comment WHERE id = :id";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $id);
+                $stmt->bindValue(":id", $id, PDO::PARAM_INT);
                 $stmt->execute();
-                $commenterId = $stmt->get_result()->fetch_assoc()['commenter_id'];
+                $commenterId = $stmt->fetchColumn();
                 return $commenterId;
-            } catch (Exception $e) {
-                throw new Exception("findCommenterIdById At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function delete($commentId) {
-            $conn = null;
+        public function delete($conn, $commentId) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "DELETE FROM comment WHERE id = ?";
+                $sql = "DELETE FROM comment WHERE id = :id";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $commentId);
+                $stmt->bindValue(":id", $commentId, PDO::PARAM_INT);
                 $stmt->execute();
-            } catch (Exception $e) {
-                throw new Exception("Delete At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function deleteByBoardId($boardId) {
-            $conn = null;
+        public function deleteByBoardId($conn, $boardId) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "DELETE FROM comment WHERE board_id = ?";
+                $sql = "DELETE FROM comment WHERE board_id = :boardId";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $commentId);
+                $stmt->bindValue(":boardId", $boardId, PDO::PARAM_INT);
                 $stmt->execute();
-            } catch (Exception $e) {
-                throw new Exception("DeleteByBoardId At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
 
-        public function updateCommenterId($newId, $oldId) {
-            $conn = null;
+        public function updateCommenterId($conn, $newId, $oldId) {
             $stmt = null;
             try {
-                $conn = DBConnectionUtil::getConnection();
-                $sql = "UPDATE comment SET commenter_id = ? WHERE commenter_id = ?";
+                $sql = "UPDATE comment SET commenter_id = :newId WHERE commenter_id = :oldId";
                 $stmt = $conn->prepare($sql);
-                $stmt->bind_param("ss", $newId, $oldId);
+                $stmt->bindValue(":newId", $newId);
+                $stmt->bindValue(":oldId", $oldId);
                 $stmt->execute();
-            } catch (Exception $e) {
-                throw new Exception("UpdateCommenterId At Comment - DB Exception 발생!");
+            } catch (PDOException $e) {
+                throw $e;
             } finally {
                 if ($stmt != null) {
-                    $stmt->close();
-                }
-
-                if ($conn != null) {
-                    $conn->close();
+                    $stmt = null;
                 }
             }
         }
