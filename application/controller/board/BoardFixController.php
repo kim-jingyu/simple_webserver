a888bd558cce9f31e7d48e2a996def7099d0d1a5
kim-jingyu <swc05161@gmail.com>
2024-02-17 08:02:59 +0000

    fix: BoardFixController

--- application/controller/board/BoardFixController.php
+++ application/controller/board/BoardFixController.php
@@ -4,14 +4,13 @@
 
     checkToken();
 
-    $boardId = filter_var(strip_tags($_POST['boardId']), FILTER_SANITIZE_SPECIAL_CHARS);
-
-    function close($message) {
+    function close($message, $boardId) {
         echo "<script>alert('$message')</script>";
         echo "<script>location.replace('/application/view/board/board_view.php?boardId=$boardId');</script>";
         exit();
     }
 
+    $boardId = filter_var(strip_tags($_POST['boardId']), FILTER_SANITIZE_SPECIAL_CHARS);
     $title = filter_var(strip_tags($_POST['title']), FILTER_SANITIZE_SPECIAL_CHARS);
     $body = filter_var(strip_tags($_POST['body']), FILTER_SANITIZE_SPECIAL_CHARS);
     $userId = filter_var(strip_tags($_POST['userId']), FILTER_SANITIZE_SPECIAL_CHARS);
@@ -21,10 +20,10 @@
         $boardController = new BoardController();
         $boardController->fixIndexBoard($boardId, $title, $body, $userId, $file);
 
-        close('수정이 완료되었습니다.');
+        close('수정이 완료되었습니다.', $boardId);
     } catch (IdNotMatchedException $e) {
-        close($e->errorMessage());
+        close($e->errorMessage(), $boardId);
     } catch (PDOException $e) {
-        close('수정에 실패했습니다!');
+        close('수정에 실패했습니다!', $boardId);
     }
 ?>
