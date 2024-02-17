b3bec6ad31f5b96d089e63ebfa2735b36eb88983
kim-jingyu <swc05161@gmail.com>
2024-02-17 13:10:20 +0000

    fix: pagenate

--- application/controller/board/BoardController.php
+++ application/controller/board/BoardController.php
@@ -19,7 +19,7 @@
                 $numPerPage = 5;
                 
                 $pageNow = $_GET['page'] ? filter_var(strip_tags(intval($_GET['page'])), FILTER_SANITIZE_SPECIAL_CHARS) : 1;
-                $blockNow = floor(($pageNow - 1) / $numPerPage) * $numPerPage;
+                $blockNow = floor(($pageNow - 1) / $numPerPage) * $numPerPage + 1;
                 $startIndexPerPage = ($pageNow - 1) * $numPerPage;
                 $sort = filter_var(strip_tags($_GET['sort']), FILTER_SANITIZE_SPECIAL_CHARS);
 
